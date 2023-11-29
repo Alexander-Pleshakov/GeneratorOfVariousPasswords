5d08603a6112c2f05b91cff4229a7f94f2c3b88d
Pleshakov-Alexandr <sanya.pleshakov.95@mail.ru>
2023-11-29 17:59:00 +0300

    added password parameters structure

--- GeneratorOfVariousPasswords/PasswordParameters.swift
+++ GeneratorOfVariousPasswords/PasswordParameters.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+public struct PasswordParameters {
+    public let p: Double
+    public let v: Double
+    public let t: Double
+    public var a: Int = 1
+    public var s: Int {
+        Int((v * t)/p)
+    }
+    public var l: Int {
+        var count = 1
+        while s < NSDecimalNumber(decimal: pow(Decimal(a), count)).intValue {
+            count += 1
+        }
+        return count
+    }
+}
