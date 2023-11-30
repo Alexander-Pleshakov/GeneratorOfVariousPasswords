52cfb53e71d9894ebef8983ccf49319dc4b57028
Pleshakov-Alexandr <sanya.pleshakov.95@mail.ru>
2023-11-30 18:44:25 +0300

    Added saving of the switch state and calculating the alphabet

--- GeneratorOfVariousPasswords/DataStructures/SelectedAlphabet.swift
+++ GeneratorOfVariousPasswords/DataStructures/SelectedAlphabet.swift
@@ -10,11 +10,38 @@ import Foundation
 let russianLetters = ["Ё", "Й", "Ц", "У", "К", "Е", "Н", "Г", "Ш", "Щ", "З", "Х", "Ъ", "Ф", "Ы", "В", "А", "П", "Р", "О", "Л", "Д", "Ж", "Э", "Я", "Ч", "С", "М", "И", "Т", "Ь", "Б", "Ю"]
 let symbols = ["!", "\"", "#", "$", "%", "&", "'", "(", ")", "*"]
 
+public var alphabet = SelectedAlphabet()
+
 public struct SelectedAlphabet {
-    public var latinLettersUppercased = false
-    public var latinLettersLowercased = false
-    public var russianLettersUppercased = false
-    public var russianLettersLowercased = false
-    public var symbols = false
-    public var numbers = false
+    public var countOfSymbols = 10
+    public var latinLettersUppercased = false {
+        didSet {
+            countOfSymbols = latinLettersUppercased ? countOfSymbols + 26 : countOfSymbols - 26
+        }
+    }
+    public var latinLettersLowercased = false {
+        didSet {
+            countOfSymbols = latinLettersLowercased ? countOfSymbols + 26 : countOfSymbols - 26
+        }
+    }
+    public var russianLettersUppercased = false {
+        didSet {
+            countOfSymbols = russianLettersUppercased ? countOfSymbols + 33 : countOfSymbols - 33
+        }
+    }
+    public var russianLettersLowercased = false {
+        didSet {
+            countOfSymbols = russianLettersLowercased ? countOfSymbols + 33 : countOfSymbols - 33
+        }
+    }
+    public var symbols = false {
+        didSet {
+            countOfSymbols = symbols ? countOfSymbols + 10 : countOfSymbols - 10
+        }
+    }
+    public var numbers = true {
+        didSet {
+            countOfSymbols = numbers ? countOfSymbols + 10 : countOfSymbols - 10
+        }
+    }
 }
--- GeneratorOfVariousPasswords/Presentation/SelectAlphabetViewController.swift
+++ GeneratorOfVariousPasswords/Presentation/SelectAlphabetViewController.swift
@@ -8,11 +8,51 @@
 import UIKit
 
 class SelectAlphabetViewController: UIViewController {
-
+    
+    @IBOutlet private weak var numbersSwitch: UISwitch!
+    @IBOutlet private weak var symbolsSwitch: UISwitch!
+    @IBOutlet private weak var russianLowercaseSwitch: UISwitch!
+    @IBOutlet private weak var russianUppercaseSwitch: UISwitch!
+    @IBOutlet private weak var latinLowercaseSwitch: UISwitch!
+    @IBOutlet private weak var latinUppercaseSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        show()
+    }
+    
+    private func show() {
+        numbersSwitch.isOn = alphabet.numbers
+        symbolsSwitch.isOn = alphabet.symbols
+        russianLowercaseSwitch.isOn = alphabet.russianLettersLowercased
+        russianUppercaseSwitch.isOn = alphabet.russianLettersUppercased
+        latinLowercaseSwitch.isOn = alphabet.latinLettersLowercased
+        latinUppercaseSwitch.isOn = alphabet.latinLettersUppercased
+    }
+    
+    @IBAction func switchLatinUppercasedValueChanged(_ sender: UISwitch) {
+        alphabet.latinLettersUppercased = sender.isOn
+    }
+    
+    @IBAction func switchLatinLowercasedValueChanged(_ sender: UISwitch) {
+        alphabet.latinLettersLowercased = sender.isOn
+    }
+    
+    @IBAction func switchRussianUppercasedValueChanged(_ sender: UISwitch) {
+        alphabet.russianLettersUppercased = sender.isOn
+    }
+    
+    @IBAction func switchRussianLowercasedValueChanged(_ sender: UISwitch) {
+        alphabet.russianLettersLowercased = sender.isOn
+    }
+    
+    @IBAction func switchSymbolsValueChanged(_ sender: UISwitch) {
+        alphabet.symbols = sender.isOn
+    }
+    
+    @IBAction func switchNumbersValueChanged(_ sender: UISwitch) {
+        alphabet.numbers = sender.isOn
     }
     
     @IBAction func buttonSelectClicked(_ sender: Any) {
